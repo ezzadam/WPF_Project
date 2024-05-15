e78a44f8c3bea63052a023bae85bbd0c90a9261b
ezzadam <ezzadam@estechs.com>
2024-05-15 17:34:19 +0800

    master

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
