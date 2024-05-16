301f114fa9abbcd4f304889a46a0a34b1ad93ba2
ezzadam <ezzadam@estechs.com>
2024-05-16 15:12:02 +0800

    1st Commit

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -11,5 +11,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FirstFlyout.IsOpen = true;
+        }
     }
 }
